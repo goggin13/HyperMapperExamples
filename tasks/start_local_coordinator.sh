88ce7dc50d38797b9b672ff9564640a5d7b8b696
Matt Goggin <goggin13@gmail.com>
2013-05-08 22:46:00 -0400

    add hyperdex starting commands

---
+++ tasks/start_local_coordinator.sh
@@ -0,0 +1,5 @@
+rm -rf ~/data/coordinator_dir
+mkdir ~/data/coordinator_dir
+cd ~/data/coordinator_dir
+hyperdex ~/data/coordinator -f -l 127.0.0.1 -p 1982
+
---
+++ tasks/start_local_daemon.sh
@@ -0,0 +1,12 @@
+rm -rf ~/data/daemon_dir
+mkdir ~/data/daemon_dir
+rm -rf ~/data/daemon_data
+mkdir ~/data/daemon_data
+
+cd ~/data/daemon_dir
+hyperdex daemon -f --listen=127.0.0.1 \
+                   --listen-port=2012 \
+                   --coordinator=127.0.0.1 \
+                   --coordinator-port=1982 \
+                   --data=~/data/daemon_data
+
